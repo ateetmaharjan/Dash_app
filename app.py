c6de3d6c937d7d2fbfa7b4fc5d6ce3992a254e7c
amahar99 <60989474+amahar99@users.noreply.github.com>
2020-05-28 14:38:07 -0500

    adding flask module

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 ###############################################################################
 # import required packages
+import flask
 import pandas as pd
 import numpy as np
 import dash_html_components as html
